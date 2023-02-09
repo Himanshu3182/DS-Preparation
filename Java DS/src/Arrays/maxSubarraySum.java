a70ee1b6430706e8a9ed69e629a0edef91682cbe
Himanshu3182 <tamta.himanshu.tamta@gmail.com>
2023-02-09 21:41:05 +0530

    Max sum subarray program added

---
+++ Java DS/src/Arrays/maxSubarraySum.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class maxSubarraySum {
+    public static void main(String[] args) {
+        int numbers[]={1,2,3,4,5,6};
+        maxSubarraySum(numbers);
+    }
+    static void maxSubarraySum(int numbers[]){
+        int maxSum=Integer.MIN_VALUE;
+        for(int i=0; i<numbers.length; i++){
+            int currSum=0;
+            for(int j=i; j<numbers.length; j++){
+                currSum=0;
+                for(int k=i; k<=j; k++){
+                    currSum+=numbers[k];
+                }
+                System.out.println(currSum);
+                if(maxSum<currSum){
+                    maxSum=currSum;
+                }
+            }
+        }
+        System.out.println("Max Sum is"+maxSum);
+    }
+}
--- Java DS/src/Arrays/printSubarrays.java
+++ Java DS/src/Arrays/printSubarrays.java
@@ -2,7 +2,25 @@ package Arrays;
 
 public class printSubarrays {
     public static void main(String[] args) {
+        int numbers[]={1,2,3,4,5,6};
+        printSubarrays(numbers);
 
+    }
+    static void printSubarrays(int numbers[]){
+        int ts=0;
+        for(int i=0; i<numbers.length; i++){
+            int start=i;
+            for(int j=i; j<numbers.length; j++){
+                int end =j;
+                for(int k=start; k<=end; k++){
+                    System.out.print(numbers[k]);
+                }
+                ts++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total subarrays="+ts);
     }
 
 }
